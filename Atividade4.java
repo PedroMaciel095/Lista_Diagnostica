49c4b658a6f4d84f85a8544a68cc98c7441c687c
PedroMaciel095 <pedrohenrique.maciel095@gmail.com>
2025-04-16 14:19:20 -0300

    Update Atividade4.java

--- Atividade4.java
+++ Atividade4.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Atividade_4{
 
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		int n, soma = 0;
+		Scanner ler = new Scanner(System.in); //utilizando o Scanner para captar informações
+		int n, soma = 0; //declarando variáveis adequadamente
 
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("digite o " + i + "° numero: "); // recolhendo dados
-			n = ler.nextInt();
-			soma = soma + n; // processamento
+			System.out.println("digite o " + i + "° numero: "); // recolhendo dados escritos pelo usuário
+			n = ler.nextInt(); //lendo oque foi informado
+			soma = soma + n; // processamento (conta)
 		}
-		System.out.println("a soma dos números é " + soma); // saída
+		System.out.println("a soma dos números é " + soma); // saída de dados para o usuário
 
 	}
 
